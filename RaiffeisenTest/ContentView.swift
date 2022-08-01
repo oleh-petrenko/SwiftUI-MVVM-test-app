88b7a2f2b6598605a8753bd92f7dffba0a29ba44
Oleh Petrenko <oleg.petrennko@gmail.com>
2022-08-01 10:10:14 +0300

    Initial Commit

---
+++ RaiffeisenTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RaiffeisenTest
+//
+//  Created by Oleh Petrenko on 01.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RaiffeisenTest/RaiffeisenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaiffeisenTestApp.swift
+//  RaiffeisenTest
+//
+//  Created by Oleh Petrenko on 01.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct RaiffeisenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
